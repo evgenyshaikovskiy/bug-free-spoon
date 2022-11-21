33488d086c0b53b306547e4c6963f4f819819586
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2022-11-21 16:10:54 +0300

    update videos(2)

--- swimming-helper/src/app/video-page/video-page.component.ts
+++ swimming-helper/src/app/video-page/video-page.component.ts
@@ -9,8 +9,24 @@ import { AppComponent } from '../app.component';
 export class VideoPageComponent {
   @Input('app') app!: AppComponent;
 
+  public brasVideoVisibility: boolean = false;
+  public butterflyVideoVisibility: boolean = false;
+  public swimmingVideoVisibility: boolean = false;
+
   public onBackClick() {
     this.app.videoVisibility = false;
     this.app.mainMenuVisibility = true;
+  }
+
+  public clickBrasVideo() {
+    this.brasVideoVisibility = !this.brasVideoVisibility;
+  }
+
+  public clickButterflyVideo() {
+    this.butterflyVideoVisibility = !this.butterflyVideoVisibility;
+  }
+
+  public clickSwimmingVideo() {
+    this.swimmingVideoVisibility = !this.swimmingVideoVisibility;
   }
 }
